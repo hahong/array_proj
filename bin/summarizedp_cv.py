c5fb14284ab93410ed28f226f948d2d259d0e6a1
Ha Hong <hahong84@gmail.com>
2011-03-31 01:08:59 -0400

    updated cv-dp

--- bin/summarizedp_cv.py
+++ bin/summarizedp_cv.py
@@ -14,6 +14,11 @@ RSEED = 0
 ABS = True     # compare absolute values
 NJOBS = -1     # maximum worker threads
 
+TOPN = 128
+NCOL = 10
+NROW = 10
+NPLOTS = 96
+PREF='cvdp'
 # ---------------------------------------------------------------------------------------
 def dprime(m_y, s_y, m_n, s_n):
     return (m_y - m_n)/np.sqrt(((s_y * s_y) + (s_n * s_n)) / 2.)
@@ -83,25 +88,187 @@ def dp_cv(db, niter=NITER, verbose=1):
     return res
 
 
+# ========================----------------------------------------------------------
+def init():
+    from matplotlib import rc
+    rc('font', **{
+        'family'          : 'serif',
+        'serif'           : ['Times New Roman'],
+        'size'            : 7})
+    rc('figure',**{
+        'figsize'           : '8,8.5',
+        'subplot.left'      : '0.04',
+        'subplot.right'     : '0.97',
+        'subplot.bottom'    : '0.065',
+        'subplot.top'       : '0.96',
+        'subplot.wspace'    : '0.25',
+        'subplot.hspace'    : '0.44',
+    })
+
+
+# ---
+def plot_all(db, ncol=NCOL, nrow=NCOL, nplots=NPLOTS, px=PREF):
+    import pylab as pl
+    from matplotlib import rc
+
+    npage = len(db) / nplots
+    ipage = 0
+    for i, ch in enumerate(db):
+        i_segment = i % nplots
+        if i_segment == 0:
+            if ipage != 0: pl.savefig(px + '_p%d.pdf' % ipage)
+            pl.figure(figsize=(18,10))
+            ipage += 1
+        pl.subplot(nrow, ncol, i_segment+1)
+        pl.hist(db[ch]['blanks'], bins=20)
+        pl.title('Ch' + str(ch), fontsize='medium')
+    return ipage
+
+
+def pick_top(db, topn, bad=[], ng=[], prndp=False, px=PREF):
+    # convert to 0-based
+    dps = [(db[ch]['m_dp'], ch-1) for ch in db if ch not in bad]
+    top = sorted(dps, reverse=True)[:topn]
+
+    # XXX: kludge
+    topch = np.array([t[1] for t in top])
+    topchng = []
+    A = topch[topch < 96]
+    M = topch[(topch >= 96) & (topch < 96*2)]
+    P = topch[topch >= 96*2]
+
+    print '0-Based selected %d (n_A=%d, n_M=%d, n_P=%d):' % (topn, len(A), len(M), len(P))
+    print A
+    print M
+    print P
+    print
+
+    for ch in ng:
+        if ch - 1 in topch: topchng.append(ch - 1)
+    print 'No good channels (0-based):', topchng
+
+    if prndp:
+        rdp = []; rch0 = []; rarr = []
+        print
+        print 'cv-dprime values:'
+        print 'dv-dpime \t ch(0-based) \t Arr'
+        for t in top:
+            ch = t[1]
+            if ch < 96: arr = 'A'
+            elif (ch >= 96) and (ch < 96*2): arr = 'M'
+            else: arr = 'P'
+
+            print t[0],'\t',ch,'\t',arr
+            rdp.append(t[0])
+            rch0.append(ch)
+            rarr.append(arr)
+        pk.dump({'dp': rdp, 'ch0': rch0, 'arr': rarr}, open(px+'_sorted128.pk', 'wb'))
+
+# ----
+def load_all(files):
+    db = {}
+    for f in files:
+        offset = len(db)
+        db0 = pk.load(open(f))
+        for ch in db0:
+            db[ch + offset] = db0[ch]
+
+    return db
+
+
+
 # ---------------------------------------------------------------------------------------
+def parse_opts(tokens, optpx='--'):
+    opts0 = []
+    args = []
+    opts = {}
+    n = len(optpx)
+
+    for token in tokens:
+        if token[:2] == optpx: opts0.append(token[n:])
+        else: args.append(token)
+
+    for opt in opts0:
+        parsed = opt.split('=')
+        key = parsed[0].strip()
+        if len(parsed) > 1:
+            cmd = parsed[1].strip()
+        else:
+            cmd = ''
+        opts[key] = cmd
+
+    return args, opts
+
 def main(rseed=RSEED):
     if len(sys.argv) < 2:
+        print 'Prep Mode:'
         print 'summarizedp_cv.py prep <d\'-CV output.pk> <combined frate.pk>'
-        print 'NOTE: the pickle files are made by combine_firrate_data.py'
+        print 'Note: input pickle files are made by combine_firrate_data.py'
+        print
+        print 'Result Formatting Mode:'
+        print 'summarizedp_cv.py res [options] <d\'-CV output A.pk> <d\'-CV output M.pk> <d\'-CV output P.pk>'
+        print 'Options:'
+        print '   --prndp            Print d\' values'
+        print '   --doplot           Plot PSTHs of blanks for each channel'
+        print '   --px=prefix        Prefix of output files'
+        print '   --bad=ch1,ch2,..   Comma separated list of 1-based bad channels'
+        print '   --ng=ch1,ch2,...   Comma separated list of 1-based not-so-good channels'
+        print '   --topn=#           Pick top # channels' 
         return
 
+    # parse options and arguments
+    args, opts = parse_opts(sys.argv[1:])
+
+    mode = args[0]
     np.random.seed(rseed)    
-    mode = sys.argv[1]
 
     # prepare d'-cv computation
     if mode == 'prep':
-        fo = sys.argv[2]
-        fi = sys.argv[3]
+        fo = args[1]
+        fi = args[2]
         print '* Output d\'-CV result:', fo
 
         db = pk.load(open(fi))
         res = dp_cv(db)
         pk.dump(res, open(fo, 'wb'))
+    # or give the results...
+    elif mode == 'res':
+        doplot, prndp, topn, px = False, False, TOPN, PREF
+        bad = []
+        ng = []
+
+        if 'doplot' in opts:
+            from matplotlib import rc, use
+            use('pdf')
+            import pylab as pl
+            init()
+            doplot = True
+            print '* DOPLOT'
+        if 'prndp' in opts:
+            prndp = True
+            print '* PRNDP'
+        if 'topn' in opts:
+            topn = int(opts('topn'))
+            print '* TOPN =', topn
+        if 'bad' in opts:
+            bad = [int(b) for b in opts['bad']]
+            print '* BAD =', bad
+        if 'ng' in opts:
+            ng = [int(b) for b in opts['ng']]
+            print '* NG =', ng
+        if 'px' in opts:
+            px = opts['px']
+            print '* PX =', px
+
+        files = args[1:]
+        db = load_all(files)
+        pick_top(db, topn, bad=bad, ng=ng, prndp=prndp, px=px)
+        
+        if doplot:
+            ipage = plot_all(db, px=px)
+            pl.savefig(px +'_p%d.pdf' % (ipage))
+            print 'Done.'
+            #pl.show()
     else:
         print 'Unrecognized mode. Aborting.'
 
