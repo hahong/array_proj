20b3e763d2891fda008bbbfd2209f8bfbdb12d13
Ha Hong <hahong84@gmail.com>
2011-07-30 21:52:04 -0400

    .

--- analysis/scheduled/10_PSTH.mh17.sh
+++ analysis/scheduled/10_PSTH.mh17.sh
@@ -2,11 +2,17 @@
 # This script is meant to be called by cronjobmaster.sh
 test "$PROJROOT" || PROJROOT=/home/array/array/
 test "$LOGDIR" || LOGDIR=$PROJROOT/analysis/scheduled/log/
-REMOTE_LOCK=array/analysis/scheduled/log/02_analyze.sh.lock
-JOBNAME=joblist/`date +%Y%m%d`_PSTH.mh17.sh
+REMOTEFILER=dicarlo2
+REMOTEUSER=array
+REMOTELOCK=array/analysis/scheduled/log/02_analyze.sh.lock
+JOBNAME=joblist/`date +%Y%m%d`_PSTH.sh
 
 ###################################################################
-# -- Merge and collect
-cd $PROJROOT/analysis/
+# if still processing the data at dicarlo2, then quits.
+ssh $REMOTEUSER@$REMOTEFILER "test -f $REMOTELOCK" && exit   
+#cd $PROJROOT/analysis/
+
+
+
 # ./04_par_merge+collect_PS_firing.py > $JOBNAME
 # parrun.py $JOBNAME 2>&1 | tee -a $LOGDIR/`date +%Y%m%d_%H%M%S`_analysis.log
