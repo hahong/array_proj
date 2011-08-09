ee11f7b5bb29abb365d328a8ee1caf464c0ef043
Ha Hong <hahong84@gmail.com>
2011-08-09 19:09:29 -0400

    .

--- analysis/scheduled/11_getdata.mh17.sh
+++ analysis/scheduled/11_getdata.mh17.sh
@@ -5,7 +5,6 @@ test "$LOGDIR" || LOGDIR=$PROJROOT/analysis/scheduled/log/
 REMOTEFILER=dicarlo2
 REMOTEUSER=array
 REMOTELOCK=array/analysis/scheduled/log/02_analyze.sh.lock
-JOBNAME=joblist/`date +%Y%m%d`_PSTH.sh
 
 ###################################################################
 # if still processing the data at dicarlo2, then quits.
@@ -14,5 +13,3 @@ ssh $REMOTEUSER@$REMOTEFILER "test -f $REMOTELOCK" && exit
 
 
 
-# ./04_par_merge+collect_PS_firing.py > $JOBNAME
-# parrun.py $JOBNAME 2>&1 | tee -a $LOGDIR/`date +%Y%m%d_%H%M%S`_analysis.log
