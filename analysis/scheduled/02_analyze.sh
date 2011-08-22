b145fa6d587bb8a56f253cc6283731132dcc1524
Ha Hong <hahong84@gmail.com>
2011-08-22 01:49:20 -0400

    .

--- analysis/scheduled/02_analyze.sh
+++ analysis/scheduled/02_analyze.sh
@@ -4,6 +4,8 @@ test "$PROJROOT" || PROJROOT=/home/array/array/
 test "$LOGDIR" || LOGDIR=$PROJROOT/analysis/scheduled/log/
 LOCK=$LOGDIR/02_analyze.sh.lock
 JOBNAME=joblist/`date +%Y%m%d`_04_merge+collect.sh
+JOBNAMEPSTH=joblist/`date +%Y%m%d`_10_plot_PSTH.sh
+LOGNAME=$LOGDIR/`date +%Y%m%d_%H%M%S`_analysis.log
 
 ###################################################################
 # -- Merge and collect
@@ -17,9 +19,15 @@ fi
 cd $PROJROOT/analysis/
 touch $LOCK   # create a lock file so that no data transfer occurs from mh17
 
-# 1. Tito
+# -- 1. Tito
+# Merge and collect
 ./04_par_merge+collect_PS_firing.py > $JOBNAME
 dirnev=data/d002_Tito/neudat_NSP2/ ./04_par_merge+collect_PS_firing.py --merge_opts='multinsp NSP2' >> $JOBNAME
+parrun.py $JOBNAME 2>&1 | tee -a $LOGNAME
 
-parrun.py $JOBNAME 2>&1 | tee -a $LOGDIR/`date +%Y%m%d_%H%M%S`_analysis.log
+# Plot PSTHs
+./10_plot_PSTH.sh > $JOBNAMEPSTH
+NJOBS=5 parrun.py $JOBNAMEPSTH 2>&1 | tee -a $LOGNAME 
+
+# -- END
 rm -f $LOCK
